eff181c20a7f7c0704703bd8248f59baa189a01d
Zhao Hongming <zhaohongming1994@gmail.com>
2022-07-27 21:58:30 +0800

    rebase commit 2

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ import java.lang.reflect.Proxy;
 public class Main {
 
     public static void main(String[] args) {
-        /*1
+        /*3
         HairInterface left = new LeftHair();
         left.draw();
 
